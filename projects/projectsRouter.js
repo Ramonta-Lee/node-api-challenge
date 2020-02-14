3ec4eef8e59750928d1ee600fada455e67085edd
unknown <viola4lfe@gmail.com>
2020-02-14 12:54:28 -0500

    PUT and DELETE functionality for projects is completed

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -37,11 +37,9 @@ router.put("/:id", validateProjectId, (req, res) => {
   const { id } = req.params;
   const changes = req.body;
   if (!changes.name || !changes.description) {
-    res
-      .status(400)
-      .json({
-        errorMessage: "Need to update the Project name AND description."
-      });
+    res.status(400).json({
+      errorMessage: "Need to update the Project name AND description."
+    });
   } else {
     ProjectsDb.update(id, changes)
       .then(update => {
@@ -52,6 +50,21 @@ router.put("/:id", validateProjectId, (req, res) => {
         res.status(500).json({ error: "Failed to update Project." });
       });
   }
+});
+
+// Delete Requests:
+
+// This deletes a Project
+router.delete("/:id", validateProjectId, (req, res) => {
+  const { id } = req.params;
+  ProjectsDb.remove(id)
+    .then(project => {
+      res.status(200).json({ message: "The Project has been removed." });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "The Project could not be removed." });
+    });
 });
 
 // custom middleware
