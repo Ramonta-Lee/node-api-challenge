b470b5d77d10a4b4d156fb33c5cfa221ce80b2f3
unknown <viola4lfe@gmail.com>
2020-02-14 13:07:49 -0500

    Able to get Actions by ID

--- projects/actionsRouter.js
+++ projects/actionsRouter.js
@@ -17,4 +17,32 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", validateProjectID, (req, res) => {
+  res.status(200).json(req.actions);
+});
+
+// custom middleware
+
+// validates an action by ID
+
+function validateProjectID(req, res, next) {
+  const { id } = req.params;
+
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        req.actions = action;
+        next();
+      } else {
+        res.status(400).json({ errorMessage: "Invalid ID" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "Server error validating project Action ID." });
+    });
+}
+
 module.exports = router;
