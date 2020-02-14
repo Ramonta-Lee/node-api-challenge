403df48688e8c5051d674759639c5a0d04ee4f73
unknown <viola4lfe@gmail.com>
2020-02-14 14:52:44 -0500

    added logger middleware and helmet global

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
-
+const helmet = require("helmet");
 // Projects & Actions Router
 const projectsRouter = require("./projects/projectsRouter.js");
 const actionsRouter = require("./projects/actionsRouter.js");
 
 const server = express();
 
+// Middleware - Global
 server.use(express.json());
+server.use(helmet());
 
 // routes - endpoints
-server.use("/api/projects", projectsRouter);
-server.use("/api/actions", actionsRouter);
+server.use("/api/projects", logger, projectsRouter);
+server.use("/api/actions", logger, actionsRouter);
 
-server.get("/", (req, res) => {
-  res.send(`<h2> Hello</h2>`);
+server.get("/", greeter, (req, res) => {
+  res.send(`<h2> Welcome ${req.me}</h2>`);
 });
+
+// custom middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} Request to ${re.originalUrl}`
+  );
+  next();
+}
+
+function greeter(req, res, next) {
+  req.me = "LEE!!!!";
+  next();
+}
 
 module.exports = server;
